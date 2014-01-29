10ffc09963668632b1ed1c63cf006bbfdcd2be41
Wesley Henrique Francisco <wesley@ipestudio.com.br>
2014-01-29 10:45:18 -0200

    jqueryMobile + phonegap + camera

--- js/index.js
+++ js/index.js
@@ -1,49 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var pictureSource;
+var destinationType;
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+	// contrutor da aplicacao
+	initialize: function() {
+		this.bindEvents();
+	},
 
-        console.log('Received Event: ' + id);
-    }
+	// Bind Event Listeners
+	//
+	// Vincular eventos que são necessários na inicialização. Eventos comuns são:
+	// 'load', 'deviceready', 'offline', e 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+
+	// deviceready Event Handler
+	//
+	// O escopo de "this" é o evento. A fim de chamar o 'receivedEvent'
+	// função, devemos explicitamente chamar 'app.receivedEvent (...);' -> exemplo anterior
+	onDeviceReady: function() {
+		pictureSource = navigator.camera.PictureSourceType;
+		destinationType = navigator.camera.DestinationType;
+	},
+
+	capturePhoto: function() {
+		navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+			quality: 50,
+			destinationType: destinationType.DATA_URL
+		});
+	},
+
+	onPhotoDataSuccess: function(imageData) {
+		var image = document.getElementById('photoImage'); // pegando elemento img
+		image.style.display = 'block'; // aplica block ao elemento image
+		image.src = "data:image/jpeg;base64," + imageData; // jogando a imagem no elemento
+	},
+
+	onFail: function(message) {
+		alert('Erro: '+message)
+	}
+
 };
